2c439ab9d5d38b5ccf61e918215e260996d88b82
Kendal Gates <leoneusg@yahoo.com>
2017-09-27 18:57:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artist, song) {
   return playlist
 }
 function removeFromPlaylist(playlist, artist) {
-  delete.playlist[artist]
+  delete playlist.artist
   return playlist
 }
