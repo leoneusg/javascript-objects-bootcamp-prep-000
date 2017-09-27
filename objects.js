45dd138e95b468781cc258de32ef8343fc3345a2
Kendal Gates <leoneusg@yahoo.com>
2017-09-27 18:55:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-var playlist = { Future: 'Stick Talk' }
+var playlist = { artist: 'song' }
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+function removeFromPlaylist(playlist, artist) {
+  delete.playlist[artist]
+  return playlist
+}
